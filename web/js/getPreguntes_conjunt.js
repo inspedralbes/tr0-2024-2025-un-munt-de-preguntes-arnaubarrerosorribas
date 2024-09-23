c807f8e647b1988de837d4f58bb504205a5124e3
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2024-09-23 10:42:54 +0200

    mostrar_pregunta_per_pregunta(front)

--- web/js/getPreguntes_conjunt.js
+++ web/js/getPreguntes_conjunt.js
@@ -1,34 +1,60 @@
+let preguntes = [];
+let jsonPreguntes = [];
+let preguntaActual = 0;
+
 fetch('http://localhost/tr0-2024-2025-un-munt-de-preguntes-arnaubarrerosorribas/back/php/getPreguntes.php')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        pintaPreguntes(data);
-    })
+        preguntes = data;
+        pintaPreguntes(preguntaActual);
+    });
 
+function pintaPreguntes(index) {
+    let htmlString = '';
+    
+    if (preguntes.length > 0) {
+        const pregunta = preguntes[index];
 
-function pintaPreguntes(data) {
-    let htmlString = ''; 
-    let preguntes = data;
+        htmlString += `<div id="divBotonesFlechas">`;
+            htmlString += `<button onclick="anterior()">  ← </button>`;
+            htmlString += `<button onclick="siguiente()"> → </button>`;
+        htmlString += `</div>`;
 
-    preguntes.forEach(pregunta => {
-
-        //console.log(pregunta.pregunta);
         htmlString += `<div id="total">`;
-        htmlString += `<p>L'ID de la pregunta es ${pregunta.id_pregunta}</p>`;
-        htmlString += `<h2>${pregunta.pregunta}</h2>`; 
-        htmlString += `<img src="${pregunta.imatge}">`;
+        htmlString += `<h2>${pregunta.pregunta}</h2>`;
+        if (pregunta.imatge) {
+            htmlString += `<img src="${pregunta.imatge}">`;
+        }
 
-        pregunta.respostes.forEach((resposta,indexResposta) => {
-            htmlString += `<button onclick="hasClicat(${pregunta.id_pregunta},${indexResposta + 1})">${resposta}</button>`;
+        pregunta.respostes.forEach((resposta) => {
+            htmlString += `<button onclick="hasClicat(${pregunta.id_pregunta},'${resposta}')">${resposta}</button>`;
         });
         htmlString += `</div>`;
-    });
+    }
 
     const divPartida = document.getElementById("preguntes");
     divPartida.innerHTML = htmlString;
 }
 
+function siguiente() {
+    if (preguntaActual < preguntes.length - 1) {
+        preguntaActual++;
+        pintaPreguntes(preguntaActual);
+    }
+}
 
-function hasClicat(pregunta,resposta){
-    console.log(`Has clicat a la pregunta ${pregunta} la resposta numero ${resposta}`);
+function anterior() {
+    if (preguntaActual > 0) {
+        preguntaActual--;
+        pintaPreguntes(preguntaActual);
+    }
+}
+
+function hasClicat(pregunta, resposta) {
+    jsonPreguntes.push({
+        pregunta: pregunta,
+        resposta: resposta
+    });
+
+    console.log(JSON.stringify(jsonPreguntes));
 }
