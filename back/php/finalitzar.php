19216b40d47f1fb46e280686fd42dd02c5679fbe
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2024-09-25 13:05:13 +0200

    PHP_mensaje_correcto

--- back/php/finalitzar.php
+++ back/php/finalitzar.php
@@ -1,18 +1,28 @@
 <?php
     $correctas = 0;
-    
-    //RESPUESTAS USUARIO
+
     $import = file_get_contents("php://input");
     $data = json_decode($import, true);
 
-    //JSON ORIGINAL
     $informacio = file_get_contents("http://localhost/tr0-2024-2025-un-munt-de-preguntes-arnaubarrerosorribas/back/listado.json");
     $preguntes_originals_decode = json_decode($informacio, true);
     $preguntes_originals = $preguntes_originals_decode['preguntes'];
 
-    
     sort($data);
-    foreach ($data as $index){
-        echo $index;
+
+    foreach ($data as $index => $pregunta) {
+        echo $pregunta['pregunta'];
+        echo (" - ");
+        echo $pregunta['resposta'];
+        echo ("---------------------------------------");
+        
+        foreach ($preguntes_originals as $indexO => $originals){
+            if ($pregunta['pregunta'] === $originals['id_pregunta']){
+                if ($pregunta['resposta'] === $originals['resposta_correcta']){
+                    echo ("La resposta de la pregunta ".$pregunta['pregunta']." es correcta");
+                } else {
+                    echo ("La resposta seleccionada es incorrecte");
+                }
+            }
+        }
     }
-    echo "Respostes ordenades correctament";
