3ff5c961dc895a1e382e9afb33c62c9b3ff94a14
arnaubarrero <a23arnbarsor@inspedralbes.cat>
2024-09-28 20:33:16 +0200

    mostrar preguntas desde SQL

--- web/js/script_sql.js
+++ web/js/script_sql.js
@@ -1,17 +1,31 @@
 fetch('http://localhost/tr0-2024-2025-un-munt-de-preguntes-arnaubarrerosorribas/back/php/getPreguntes_sql.php')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         preguntes = data;
-        pintaPreguntes(preguntaActual);
+        pintaPreguntes();
     })
 
-function pintaPreguntes(index) {
+
+    function pintaPreguntes() {
     let htmlString = '';
 
-    const pregunta = preguntes[index];
-    htmlString = `<p> ${pregunta.texto} </p>`;
+    preguntes.forEach((pregunta) => {
+        htmlString += `<div class="pregunta">`;
+        htmlString += `<p>Pregunta ${pregunta.id_pregunta}: ${pregunta.enunciat}</p>`;
+        htmlString += `<ul>`;
+        
+        pregunta.opcions.forEach(opcio => {
+            htmlString += `<li><button onclick="hasClicat('${opcio}')">${opcio}</button></li>`;
+        });
+        
+        htmlString += `</ul>`;
+        htmlString += `</div>`;
+    });
 
     const divPartida = document.getElementById("preguntes");
     divPartida.innerHTML = htmlString;
+}
+
+function hasClicat(pregunta){
+    console.log(`${pregunta}`);
 }
