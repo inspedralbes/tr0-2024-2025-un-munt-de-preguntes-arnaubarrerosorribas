76b76310078f7bdadb47ce7cbad74d593d68b254
arnaubarrero <a23arnbarsor@inspedralbes.cat>
2024-09-30 17:09:09 +0200

    admin_02

--- web/js/admin.js
+++ web/js/admin.js
@@ -5,18 +5,22 @@ fetch('http://localhost/tr0-2024-2025-un-munt-de-preguntes-arnaubarrerosorribas/
         pintaPreguntes();
     });
 
-function pintaPreguntes(){
+function pintaPreguntes() {
     let htmlString = '';
 
+    htmlString += `<button id="addQuestionBtn">Afegir una nova pregunta</button>`;
 
     preguntes.forEach(entrada => {
+        htmlString += `<div class="question-entry">`;
         htmlString += `<p>${entrada.id_pregunta}</p>`;
-        htmlString += `<p>${entrada.enunciat}</p>`;
+        htmlString += `<b>Pregunta: ${entrada.enunciat}</b><br>`;
+        htmlString += `<b>Resposta Correcta: ${entrada.pCorrecte}</b>`;
         htmlString += `<p>${entrada.p1}</p>`;
         htmlString += `<p>${entrada.p2}</p>`;
         htmlString += `<p>${entrada.p3}</p>`;
-        htmlString += `<p>${entrada.pCorrecte}</p>`;
-        htmlString += `<p>${entrada.imatge}</p>`;
+        htmlString += `<p style="border-bottom: 2px solid black;">${entrada.imatge}</p>`;
+        htmlString += `<button onclick="editEntry(${entrada.id_pregunta})">Editar</button>`;
+        htmlString += `</div>`;
     });
 
     const divPartida = document.getElementById("total");
